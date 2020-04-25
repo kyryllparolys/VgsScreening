bfbc63770760b86831cbf8e3ec097651406cb8e6
Kyryll Parolys <kyryllpar@gmail.com>
2020-04-25 06:54:23 +0300

    Finished form type testing.

--- tests/TestInboundConnections.py
+++ tests/TestInboundConnections.py
@@ -10,7 +10,7 @@ class TestInboundConnectionsJson(unittest.TestCase):
 
     def test_json(self):
         headers = {'Content-type': 'application/json'}
-        data = '{"account_number": "ACC00000000000000000"}'
+        data = '{"account_number": "{}"}'.format(self.account_number)
 
         r = requests.post('https://tntsfeqzp4a.sandbox.verygoodproxy.com/post', headers=headers,
                           data=data)  # TODO: Change to format
@@ -20,10 +20,17 @@ class TestInboundConnectionsJson(unittest.TestCase):
         self.assertNotEqual(tokenized, self.account_number)
 
 
-class TestInboundConnectionsXml(unittest.TestCase):
+class TestInboundConnectionForm(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.account_number = "ACC00000000000000001"
+        self.account_number = 'ACC00000000000000000'
 
-    def test_xml(self):
-        headers = {'Content-type': 'application/xml'}
+    def test_form(self):
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = f'account_number={self.account_number}'
+
+        r = requests.post('https://tnts4a6y6fb.sandbox.verygoodproxy.com/post', headers=headers, data=data)
+        tokenized = r.json()['form'].get('account_number')
+
+        self.assertEqual(r.status_code, 200)
+        self.assertNotEqual(tokenized, self.account_number)
