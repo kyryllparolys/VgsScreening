fb5712c8b97d83d560a2f2b2f30c6e9a6bbbe432
Kyryll Parolys <kyryllpar@gmail.com>
2020-04-25 18:18:24 +0300

    Finished xml part.

--- tests/TestInboundConnections.py
+++ tests/TestInboundConnections.py
@@ -1,4 +1,5 @@
 import unittest
+import xml.etree.ElementTree as ET
 
 import requests
 
@@ -10,13 +11,31 @@ class TestInboundConnectionsJson(unittest.TestCase):
 
     def test_json(self):
         headers = {'Content-type': 'application/json'}
-        data = '{"account_number": "{}"}'.format(self.account_number)
+        data = '{"account_number": "ACC00000000000000000"}'
 
         r = requests.post('https://tntsfeqzp4a.sandbox.verygoodproxy.com/post', headers=headers,
                           data=data)  # TODO: Change to format
-        tokenized = eval(r.json()['data']).get('account_number')
-
         self.assertEqual(r.status_code, 200)
+
+        tokenized = eval(r.json()['data']).get('account_number')
+        self.assertNotEqual(tokenized, self.account_number)
+
+
+class TestInboundConnectionsXml(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.account_number = 'ACC00000000000000000'
+
+    def test_xml(self):
+        headers = {'Content-type': 'application/xml'}
+        data = f'<account_number>{self.account_number}</account_number>'
+
+        r = requests.post('https://tnts4a6y6fb.sandbox.verygoodproxy.com/post', headers=headers, data=data)
+
+        root = ET.fromstring(r.json()['data'])
+        self.assertEqual(r.status_code, 200)
+
+        tokenized = root.text
         self.assertNotEqual(tokenized, self.account_number)
 
 
@@ -27,10 +46,10 @@ class TestInboundConnectionForm(unittest.TestCase):
 
     def test_form(self):
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        data = f'account_number={self.account_number}'
+        data = 'account_number=ACC00000000000000000'
 
         r = requests.post('https://tnts4a6y6fb.sandbox.verygoodproxy.com/post', headers=headers, data=data)
-        tokenized = r.json()['form'].get('account_number')
-
         self.assertEqual(r.status_code, 200)
+
+        tokenized = r.json()['form'].get('account_number')
         self.assertNotEqual(tokenized, self.account_number)
